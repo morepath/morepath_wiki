ca9073f5abd31f5bba1a49280cfdedad1d87622f
taschini <taschini@users.noreply.github.com>
2016-06-04 15:23:06 +0200

    Tuning metadata after prerelase checks.

--- setup.py
+++ setup.py
@@ -3,29 +3,26 @@
 import io
 from setuptools import setup, find_packages
 
-name = 'morepath_wiki'
-description = (
-    'Morepath Wiki inspired by web-micro-battle'
-)
-long_description = (
-    io.open('README.rst', encoding='utf-8').read() + '\n\n' +
-    io.open('CHANGES.rst', encoding='utf-8').read())
-version = '0.1.dev0'
 
 setup(
-    name=name,
-    version=version,
-    description=description,
-    long_description=long_description,
+    name='morepath_wiki',
+    version='0.1.dev0',
+    description=(
+        'A wiki demo for Morepath, based on the '
+        'web micro-framework battle by Richard Jones'),
+    long_description=(
+        io.open('README.rst', encoding='utf-8').read() + '\n\n' +
+        io.open('CHANGES.rst', encoding='utf-8').read()),
     author='Morepath developers',
     author_email='morepath@googlegroups.com',
     license="BSD",
-    url="https://github.com/morepath/morepath_rest_dump_load",
+    url="https://github.com/morepath/morepath_wiki",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
+    keywords='morepath demo',
     install_requires=[
-        'morepath>=0.14',
+        'morepath>=0.14,==0.*',
         # 'html', This library has been bundled within the source code.
     ],
     extras_require=dict(
@@ -45,7 +42,9 @@ setup(
         'Environment :: Web Environment',
         'License :: OSI Approved :: BSD License',
         'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ]
